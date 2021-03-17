345451b315c6140b8e84812b0961cced9f42d3af
hadi yusri <muhdahmad925@gmail.com>
2021-03-17 11:53:57 -0700

    update t.test for nth time

--- R/my_t.test.R
+++ R/my_t.test.R
@@ -1,8 +1,27 @@
+#' the t-test calculator
+#'
+#' This function calculates values associated to the t-test such as the
+#' test statistics, p-value, degree of freedom, and the conclusion on
+#' whether we reject or fail to reject the null hypothesis tested from
+#' lifeExp data in the gapminder package.
+#'
+#' @param x the numeric vector of lifeExp pulled from the gapminder data
+#' @param alternative the one-sided or two-sided test
+#' @param mu the mean value tested given from the null hypothesis (mu = 60)
+#' @keywords hypothesis
+#'
+#' @return A list indicating test statistics, p-value, degree of freedom,
+#' and the conclusion on whether we reject or fail to reject the null hypothesis
+#' tested from lifeExp data in the gapminder package.
+#'
+#' @examples
+#' my_t.test(x, "less", 60)
+#' my_t.test(x, "greater", 60)
+#' my_t.test(x, "two.sided", 60)
+#'
+#' @export
 library(gapminder)
-# The my_t.test function
-# Parameter: x = numeric vector of data, alternative = character string,
-#           mu = number indicating the null hypothesis value of the mean
-# Return: a list with elements test_stat, df, alternative, p_val
+x <- my_gapminder %>% pull(lifeExp)
 my_t.test <- function(x, alternative, mu) {
 
   # calculate standard error
@@ -16,17 +35,25 @@ my_t.test <- function(x, alternative, mu) {
   if (alternative == "less") {
     p_val <- pt(t_stat, deg_freedom, lower.tail = TRUE)
   } else if (alternative == "greater") {
-    p_val <- pt(t_stat, deg_freedom, lower.tail = FALSE)
+    p_val <- 1 - pt(t_stat, deg_freedom, lower.tail = FALSE)
   } else if (alternative == "two.sided") {
     p_val <- pt(abs(t_stat), deg_freedom, lower.tail = FALSE) * 2
   } else {
     stop("Error; parameter 'alternative should be either two.sided,
          less, or greater")
   }
+
+  if(p_val < 0.05) {
+    message <- "reject the null"
+  } else {
+    message <- "fail to reject the null"
+  }
+
   # create a returned list
   return_list <- list("test_stat"   = t_stat,
                       "df"          = deg_freedom,
                       "alternative" = alternative,
-                      "p_val"       = p_val)
+                      "p_val"       = p_val,
+                      "conclusion"  = message)
   return(return_list)
 }
